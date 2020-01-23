ec43a9b9adf4a1d5fc159db3e92f70ec4f8121a4
Nicolas PERNOUD <github@ninico.fr>
2020-01-23 20:45:08 +0100

    revert: CSP/frame-src

--- pkg/security/middlewares.go
+++ pkg/security/middlewares.go
@@ -21,7 +21,7 @@ func CorsMiddleware(next http.Handler, allowedOrigin string) http.Handler {
 func WebSecurityMiddleware(next http.Handler, source string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Strict-Transport-Security", "max-age=63072000")
-		w.Header().Set("Content-Security-Policy", fmt.Sprintf("default-src %[1]v 'self'; img-src %[1]v blob: 'self'; script-src 'self' %[1]v; style-src 'self' 'unsafe-inline'; frame-src %[1]v http://%[1]v; frame-ancestors %[1]v", source))
+		w.Header().Set("Content-Security-Policy", fmt.Sprintf("default-src %[1]v 'self'; img-src %[1]v blob: 'self'; script-src 'self' %[1]v; style-src 'self' 'unsafe-inline'; frame-src %[1]v; frame-ancestors %[1]v", source))
 		w.Header().Set("X-Frame-Options", "SAMEORIGIN")
 		w.Header().Set("X-XSS-Protection", "1; mode=block")
 		w.Header().Set("Referrer-Policy", "strict-origin")
