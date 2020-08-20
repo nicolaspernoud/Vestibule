25b10a2272aeda78d77bda21b35283bd9d7a6e06
Nicolas Pernoud <github@ninico.fr>
2020-08-20 14:37:33 +0200

    fix: behavior when accessing directly login route while already logged

--- web/components/login/login.js
+++ web/components/login/login.js
@@ -11,52 +11,58 @@ let password_field;
 let login_inmemory;
 let login_icon;
 
-export function mount(where) {
-  Navbar.CreateMenu();
+export async function mount(where) {
   mountpoint = where;
-  document.getElementById(mountpoint).innerHTML = /* HTML */ `
-    <div class="columns">
-      <div class="column is-half is-offset-one-quarter">
-        <div class="card">
-          <div class="card-content">
-            <div class="field">
-              <p class="control has-icons-left has-icons-right">
-                <input id="login-login" class="input" type="text" placeholder="Login" />
-                <span class="icon is-small is-left">
-                  <i class="fas fa-user"></i>
-                </span>
-              </p>
+  let user = await Auth.GetUser();
+  if (user !== undefined) {
+    document.getElementById(mountpoint).innerHTML = "";
+    location.hash = "#";
+  } else {
+    Navbar.CreateMenu();
+    document.getElementById(mountpoint).innerHTML = /* HTML */ `
+      <div class="columns">
+        <div class="column is-half is-offset-one-quarter">
+          <div class="card">
+            <div class="card-content">
+              <div class="field">
+                <p class="control has-icons-left has-icons-right">
+                  <input id="login-login" class="input" type="text" placeholder="Login" />
+                  <span class="icon is-small is-left">
+                    <i class="fas fa-user"></i>
+                  </span>
+                </p>
+              </div>
+              <div class="field">
+                <p class="control has-icons-left">
+                  <input id="login-password" class="input" type="password" placeholder="Password" />
+                  <span class="icon is-small is-left">
+                    <i class="fas fa-lock"></i>
+                  </span>
+                </p>
+              </div>
             </div>
-            <div class="field">
-              <p class="control has-icons-left">
-                <input id="login-password" class="input" type="password" placeholder="Password" />
-                <span class="icon is-small is-left">
-                  <i class="fas fa-lock"></i>
-                </span>
-              </p>
-            </div>
+            <footer class="card-footer">
+              ${loginModes.inmemory
+                ? /* HTML */ `
+                    <a id="login-inmemory" class="card-footer-item">
+                      <span class="icon" id="login-icon"><i class="fas fa-key"></i></span>Login
+                    </a>
+                  `
+                : ""}
+              ${loginModes.oauth2
+                ? /* HTML */ `
+                    <a id="login-oauth2" class="card-footer-item" href="/OAuth2Login">
+                      <span class="icon"><i class="fab fa-keycdn"></i></span>Login with OAuth2
+                    </a>
+                  `
+                : ""}
+            </footer>
           </div>
-          <footer class="card-footer">
-            ${loginModes.inmemory
-              ? /* HTML */ `
-                  <a id="login-inmemory" class="card-footer-item">
-                    <span class="icon" id="login-icon"><i class="fas fa-key"></i></span>Login
-                  </a>
-                `
-              : ""}
-            ${loginModes.oauth2
-              ? /* HTML */ `
-                  <a id="login-oauth2" class="card-footer-item" href="/OAuth2Login">
-                    <span class="icon"><i class="fab fa-keycdn"></i></span>Login with OAuth2
-                  </a>
-                `
-              : ""}
-          </footer>
         </div>
       </div>
-    </div>
-  `;
-  registerModalFields();
+    `;
+    registerModalFields();
+  }
 }
 
 function registerModalFields() {
